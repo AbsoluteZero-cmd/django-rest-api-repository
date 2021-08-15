e67ed3987b3e72692d7f9d0bc327e16dfd823f14
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2021-08-15 21:08:09 +0600

    Added api tests, api permissions

--- proj/api/tests/test_api.py
+++ proj/api/tests/test_api.py
@@ -1,29 +1,29 @@
-# from django.contrib.auth.models import User
-# from django.urls import reverse
-# from rest_framework import status
-# from rest_framework.test import APITestCase
-#
-# from api.serializers import QuestionSerializer
-# from questions.models import Question
-#
-#
-# class QuestionApiTestCase(APITestCase):
-#
-#     def setUp(self):
-#         self.user_1 = User.objects.create_user('Chevy Chase', 'chevy@chase.com', 'chevyspassword')
-#         self.user_2 = User.objects.create_user('Jim Carrey', 'jim@carrey.com', 'jimspassword')
-#         self.user_3 = User.objects.create_user('Dennis Leary', 'dennis@leary.com', 'denisspassword')
-#
-#         self.question1 = Question.objects.create(author=self.user_1, title='title 1', text='text 1')
-#         self.question2 = Question.objects.create(author=self.user_2, title='title 2', text='text 5')
-#         self.question3 = Question.objects.create(author=self.user_3, title='title 3', text='text 1')
-#
-#     def test_get(self):
-#         url = reverse('question-list')
-#         response = self.client_get(url)
-#         serializer_data = QuestionSerializer([self.question1, self.question2, self.question3], many=True).data
-#         self.assertEqual(status.HTTP_200_OK, response.status_code)
-#         self.assertEqual(serializer_data, response.data)
-#
-#
-#
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from api.serializers import QuestionSerializer
+from questions.models import Question
+
+
+class QuestionApiTestCase(APITestCase):
+
+    def setUp(self):
+        self.user_1 = User.objects.create_user('Chevy Chase', 'chevy@chase.com', 'chevyspassword')
+        self.user_2 = User.objects.create_user('Jim Carrey', 'jim@carrey.com', 'jimspassword')
+        self.user_3 = User.objects.create_user('Dennis Leary', 'dennis@leary.com', 'denisspassword')
+
+        self.question1 = Question.objects.create(author=self.user_1, title='title 1', text='text 1')
+        self.question2 = Question.objects.create(author=self.user_2, title='title 2', text='text 5')
+        self.question3 = Question.objects.create(author=self.user_3, title='title 3', text='text 1')
+
+    def test_get(self):
+        url = reverse('api:question-list')
+        response = self.client.get(url)
+        serializer_data = QuestionSerializer([self.question3, self.question2, self.question1], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+
+
--- proj/api/views.py
+++ proj/api/views.py
@@ -1,8 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
+from api.permissions import IsAuthorOrStaffOrReadOnly
 from api.serializers import QuestionSerializer
 from questions.models import Question
 
@@ -11,7 +12,11 @@ class QuestionViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthorOrStaffOrReadOnly]
     filter_fields = ['title']
     search_fields = ['title', 'text']
     ordering_fields = ['update_date']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['author'] = self.request.user
+        serializer.save()
